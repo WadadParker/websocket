ada85197fc152ebc7fca1210f83a9ca1f1f14484
Wadad Parker <wadadparker5@gmail.com>
2024-04-06 20:13:48 +0530

    feat: websocket connection

---
+++ dist/index.js
@@ -0,0 +1,54 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const ws_1 = __importStar(require("ws"));
+const http_1 = __importDefault(require("http"));
+//  http is native code for browser, we use express for middlewares ka easy routing
+const server = http_1.default.createServer(function (request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.end("hi there");
+});
+// Whenever we have websocket server, it still uses a HTTP server
+// We can create the HTTP server via express as welll
+// The below logic remains same for any websocket with any lib any platform
+const wss = new ws_1.WebSocketServer({ server });
+// wss is websocket server, ws is websocket
+wss.on('connection', function connection(ws) {
+    ws.on('error', console.error);
+    ws.on('message', function message(data, isBinary) {
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === ws_1.default.OPEN) {
+                client.send(data, { binary: isBinary });
+            }
+        });
+    });
+    ws.send('Hello! Message From Server!!');
+});
+server.listen(8080, function () {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import WebSocket, { WebSocketServer } from 'ws';
 import http from 'http';
 
+//  http is native code for browser, we use express for middlewares ka easy routing
+
 const server = http.createServer(function(request: any, response: any) {
     console.log((new Date()) + ' Received request for ' + request.url);
     response.end("hi there");
 });
 
+// Whenever we have websocket server, it still uses a HTTP server
+// We can create the HTTP server via express as welll
+
+// The below logic remains same for any websocket with any lib any platform
 const wss = new WebSocketServer({ server });
 
+// wss is websocket server, ws is websocket
 wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
 
